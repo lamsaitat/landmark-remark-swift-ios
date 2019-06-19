e456865ea23098d757b97d0e99c8c86b53d0054f
Kelvin Lam <lamsaitat@gmail.com>
2019-06-19 14:36:08 +1000

    Initial Commit

---
+++ LandmarkRemark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkRemark
+//
+//  Created by Sai Tat Lam on 19/6/19.
+//  Copyright © 2019 Sai Tat Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
