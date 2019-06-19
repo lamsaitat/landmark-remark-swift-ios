7852af5fc663f210964d83998de028836ee633e3
Kelvin Lam <lamsaitat@gmail.com>
2019-06-19 15:29:28 +1000

    Basic implementation of the user struct.

---
+++ LandmarkRemark/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  LandmarkRemark
+//
+//  Created by Sai Tat Lam on 19/6/19.
+//  Copyright © 2019 Sai Tat Lam. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let uid: String
+    let email: String
+    let username: String
+}
