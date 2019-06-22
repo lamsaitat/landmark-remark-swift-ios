633dbb375a888a7f9a826c186b935f89419cb843
Kelvin Lam <lamsaitat@gmail.com>
2019-06-22 23:27:19 +1000

    Implemented table view cell methods for landmark list vc.

--- LandmarkRemark/ViewControllers/Landmark/ListView/LandmarkListViewController.swift
+++ LandmarkRemark/ViewControllers/Landmark/ListView/LandmarkListViewController.swift
@@ -9,82 +9,45 @@
 import UIKit
 
 class LandmarkListViewController: UITableViewController {
-
+    var viewModel: LandmarkListViewModel! = LandmarkListViewModel(with: [Note]()) {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return viewModel.numberOfSections
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return viewModel.numberOfRows(inSection: section)
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath) as? NoteCell else {
+            return UITableViewCell()
+        }
+        if let cvm = viewModel.cellViewModel(at: indexPath) {
+            configure(cell, withCellViewModel: cvm)
+        }
+        
         return cell
     }
-    */
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+// MARK: - Cell configuration
+extension LandmarkListViewController {
+    func configure(_ cell: NoteCell, withCellViewModel cellViewModel: NoteCellViewModel) {
+        cell.messageLabel.text = cellViewModel.messageLabelDisplayString
+        cell.locationLabel.text = cellViewModel.locationLabelDisplayString
+        cell.authorLabel.text = cellViewModel.authorLabelDisplayString
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- LandmarkRemarkTests/ViewControllers/Landmark/ListView/LandmarkListViewControllerTests.swift
+++ LandmarkRemarkTests/ViewControllers/Landmark/ListView/LandmarkListViewControllerTests.swift
@@ -11,9 +11,16 @@ import XCTest
 
 class LandmarkListViewControllerTests: BaseTestCase {
     var vc: LandmarkListViewController!
-    
+    var notes: [Note] {
+        return [
+            Note(latitude: wynyardCoord.latitude, longitude: wynyardCoord.longitude, message: "This is message 0", authorUid: "authorUid-1", authorDisplayName: "TestUser1"),
+            Note(latitude: wynyardCoord.latitude, longitude: wynyardCoord.longitude, message: "This is message 1", authorUid: "authorUid-1", authorDisplayName: "TestUser1"),
+            Note(latitude: wynyardCoord.latitude, longitude: wynyardCoord.longitude, message: "This is message 2", authorUid: "authorUid-1", authorDisplayName: "TestUser1")
+        ]
+    }
     override func setUp() {
         vc = createLandmarkListViewController()
+        vc.viewModel = LandmarkListViewModel(with: notes)
         vc.loadViewIfNeeded()
     }
 
@@ -24,5 +31,46 @@ class LandmarkListViewControllerTests: BaseTestCase {
     func testLoadView() {
         XCTAssertNotNil(vc)
         XCTAssertNotNil(vc.tableView)
+    }
+    
+    func testNumberOfSections() {
+        XCTAssertEqual(vc.numberOfSections(in: vc.tableView), vc.viewModel.numberOfSections)
+    }
+    
+    func testNumberOfRows() {
+        for section in 0..<vc.numberOfSections(in: vc.tableView) {
+            XCTAssertEqual(vc.tableView(vc.tableView, numberOfRowsInSection: section), vc.viewModel.numberOfRows(inSection: section))
+        }
+    }
+    
+    func testConfigureNoteCell() {
+        let indexPath = IndexPath(row: 0, section: 0)
+        guard let cell = vc.tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath) as? NoteCell else {
+            return XCTFail("Unable to create NoteCell instance to run the test.")
+        }
+        
+        for note in notes {
+            let cvm = NoteCellViewModel(with: note)
+            vc.configure(cell, withCellViewModel: cvm)
+            XCTAssertEqual(cell.authorLabel.text, cvm.authorLabelDisplayString)
+            XCTAssertEqual(cell.messageLabel.text, cvm.messageLabelDisplayString)
+            XCTAssertEqual(cell.locationLabel.text, cvm.locationLabelDisplayString)
+        }
+    }
+    
+    func testCellForRowAtIndexPath() {
+        for section in 0..<vc.numberOfSections(in: vc.tableView) {
+            for row in 0..<vc.tableView(vc.tableView, numberOfRowsInSection: section) {
+                let indexPath = IndexPath(row: row, section: section)
+                guard let cell = vc.tableView(vc.tableView, cellForRowAt: indexPath) as? NoteCell else {
+                    return XCTFail("Incorrect cell type returned.")
+                }
+                let note = notes[row]
+                let cvm = NoteCellViewModel(with: note)
+                XCTAssertEqual(cell.authorLabel.text, cvm.authorLabelDisplayString)
+                XCTAssertEqual(cell.messageLabel.text, cvm.messageLabelDisplayString)
+                XCTAssertEqual(cell.locationLabel.text, cvm.locationLabelDisplayString)
+            }
+        }
     }
 }
